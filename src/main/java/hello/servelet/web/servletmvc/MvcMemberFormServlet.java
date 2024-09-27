309bd55298d9ef2883dc5066c1cc6bd1c106d1a0
Ramel <dev_20@naver.com>
2024-09-27 22:22:42 +0900

    FrontController-v1

--- src/main/java/hello/servelet/web/servletmvc/MvcMemberFormServlet.java
+++ src/main/java/hello/servelet/web/servletmvc/MvcMemberFormServlet.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 public class MvcMemberFormServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String viewPath = "/WEB-INF/views/new-form.jsp";
+        String viewPath = "/WEB-INF/views/new-form.jsp"; // WEB-INF 안에 파일들은 URL을 직접 입력하면 못들어가고, 무조컨 컨트롤러 통해서 가능!
         RequestDispatcher requestDispatcher = req.getRequestDispatcher(viewPath);
         requestDispatcher.forward(req, resp);
     }
